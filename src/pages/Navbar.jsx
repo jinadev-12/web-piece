f1abbc448c4ac5a9c2495c4d26749ea438cf6f7f
akhilsm <primeorwott@gmail.com>
2025-10-18 20:20:38 +0530

    navbar third touch

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -18,12 +18,17 @@ function Navbar() {
     <>
       {/* Mobile Promo Banner */}
       <div className="md:hidden text-xs bg-blue-800 text-center py-2 font-secondary text-white">
-        70% off on Advanced JavaScript Course —{" "}
-        <span className="underline">Tap to view</span>
+        New web projects just released —{" "}
+        <span className="underline">Check them out</span>
       </div>
 
       {/* Navbar */}
-      <nav className="bg-[lch(16.1_10.6_263.6)] text-white relative font-secondary shadow-[0_40px_60px_-10px_rgba(0,0,0,0.5)]">
+      <nav
+        className="
+      bg-[lch(16.1_10.6_263.6)]
+      bg-[#202f44]
+       text-white relative font-secondary shadow-[0_40px_50px_-5px_rgba(0,0,0,0.1)]"
+      >
         {/* Container */}
         <div className="flex justify-between h-16 items-center px-6">
           {/* Logo */}
@@ -45,7 +50,7 @@ function Navbar() {
           {/* Alert Section (Desktop) */}
           <div className="hidden md:flex justify-between items-center gap-4">
             <p className="text-xs">
-              Up to 70% off on Advanced JavaScript Course
+              Exclusive drop: new web projects just released
             </p>
             <button className="underline text-sm text-blue-500 hover:text-blue-400 transition">
               Tap to view!
@@ -56,10 +61,10 @@ function Navbar() {
           <div className="hidden md:block">
             <a
               href="#"
-              className="text-sm p-[6px] px-6 rounded-full text-white font-medium
+              className="text-sm py-[6px] px-6 rounded text-white font-normal
               bg-[#019eff] 
-              hover:from-blue-500 hover:to-indigo-500
-              transition-all duration-300 shadow-md hover:shadow-lg"
+              hover:bg-[rgb(56,179,255)] 
+              transition-all duration-200 shadow-md hover:shadow-lg"
             >
               Contact
             </a>
@@ -68,31 +73,24 @@ function Navbar() {
 
         {/* Sidebar (Mobile Only) */}
         <div
-          className={`fixed top-0 left-0 h-full w-64 bg-[#111733] text-white transform ${
+          className={`fixed top-0 left-0 h-full w-64 bg-[lch(16.1_10.6_263.6)] text-white transform ${
             sidebarOpen ? "translate-x-0" : "-translate-x-full"
           } transition-transform duration-300 ease-in-out z-50 md:hidden`}
         >
-          <ul className="flex flex-col h-full gap-4 mt-6">
+          <ul className="flex flex-col h-full gap-4">
             {links.map((link) => (
               <NavLink
                 key={link.path}
                 to={link.path}
                 className={({ isActive }) =>
-                  `flex items-center rounded-l-full py-2 pl-4 pr-3 mx-3 transition-all duration-300 ${
+                  `flex items-center h-12 pl-5 transition-colors duration-300 ${
                     isActive
-                      ? "bg-gradient-to-r from-blue-600 via-indigo-600 to-purple-600 shadow-md"
-                      : "hover:bg-[#1b244a]"
+                      ? "bg-gradient-to-r from-[#019eff]"
+                      : "bg-transparent"
                   }`
                 }
               >
-                <div
-                  className={`h-12 w-[4px] rounded-tr-lg rounded-br-lg mr-5 transition-all duration-300 ${
-                    link.path === window.location.pathname
-                      ? "bg-gradient-to-b from-blue-400 to-purple-400"
-                      : "bg-[#26ffe3]"
-                  }`}
-                ></div>
-
+                {/* <div className="h-12 w-[4px] rounded-tr-lg rounded-br-lg bg-[#26ffe3] mr-5"></div> */}
                 <div className="flex items-center gap-2">
                   <img
                     src="https://greatstack.dev/_next/static/media/projects.3710561f.png"
